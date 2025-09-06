c6f81346b63af309bad00f51b9c37a69134e51b0
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-06 10:13:19 +0200

    feat: fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+
+  // faccio la chiamata axios per recuperare l'array e inserirli in data
+  const fetchData = () => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((resp) => setData(resp.data));
+  };
+
+  // quindi al caricamento della componente avverà la chiamata
+  useEffect(fetchData, []);
+
   return (
     <>
       <h1>ciao</h1>
